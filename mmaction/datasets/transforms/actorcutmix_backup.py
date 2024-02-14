a1aebf657f04a40d25dce21c90948aa592d53ea7
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-02-14 22:53:56 +0900

    Implementing fast file loading

--- mmaction/datasets/transforms/actorcutmix_backup.py
+++ mmaction/datasets/transforms/actorcutmix_backup.py
@@ -1,4 +1,3 @@
-import json
 from pathlib import Path
 from random import choice, random
 
@@ -8,19 +7,20 @@ from mmcv.transforms import BaseTransform
 
 @TRANSFORMS.register_module()
 class ActorCutMix(BaseTransform):
-    def __init__(self, root, file_list, prob):
+    def __init__(self, root, prob):
         self.acm_root = Path(root)
         self.prob = prob
 
-        with open(file_list) as f:
-            self.file_list = json.load(f)
-
     def transform(self, results):
         if random() > self.prob:
-            file_path = Path(results["filename"])
-            action = file_path.parent.name
-            options = [f for f in self.file_list[action] if file_path.stem in f]
-            video_pick = self.acm_root / choice(options)
+            filepath = Path(results["filename"])
+            action = filepath.parent.name
+            action_dir = self.acm_root / action
+            options = [
+                f for f in action_dir.iterdir() if str(f.stem).startswith(filepath.stem)
+            ]
+
+            video_pick = choice(options)
             results["filename"] = str(video_pick)
 
         return results
