d765bcae82e9aaf0ee44f13f4c7d7f7ab3949a12
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-03-28 05:24:48 +0900

    Added CSN configs

--- configs/recognition/csn/ircsn_ig65m-pretrained-r152_8xb12-32x2x1-58e_ucf101-rgb-actorcutmix-p0.5-mmr0.0.py
+++ configs/recognition/csn/ircsn_ig65m-pretrained-r152_8xb12-32x2x1-58e_ucf101-rgb-actorcutmix-p0.5-mmr0.0.py
@@ -10,13 +10,21 @@ model = dict(
 
 # dataset settings
 dataset_type = 'VideoDataset'
+dataset = 'ucf101'
+mix_mode = 'actorcutmix'
+min_mask_ratio = 0.0
+relevancy_model = 'all-mpnet-base-v2'
+relevancy_thresh = 0.5
+num_workers = 12
+
 data_root = 'data/ucf101/videos'
+video_root = f'data/{dataset}/REPP/{mix_mode}/mix/{relevancy_model}/{relevancy_thresh}'
+video_list = f'{video_root}/list.json'
 data_root_val = f'data/{dataset}/videos'
 split = 1  # official train/test splits. valid numbers: 1, 2, 3
 ann_file_train = f'data/{dataset}/{dataset}_train_split_{split}_videos.txt'
 ann_file_val = f'data/{dataset}/{dataset}_val_split_{split}_videos.txt'
 ann_file_test = f'data/{dataset}/{dataset}_val_split_{split}_videos.txt'
-num_workers = 12
 
 # file_client_args = dict(
 #      io_backend='petrel',
@@ -25,6 +33,7 @@ num_workers = 12
 file_client_args = dict(io_backend='disk')
 
 train_pipeline = [
+    dict(type='ActorCutMix', root=video_root, file_list=video_list, prob=0.5, min_mask_ratio=min_mask_ratio),
     dict(type='DecordInit', **file_client_args),
     dict(type='SampleFrames', clip_len=32, frame_interval=2, num_clips=1),
     dict(type='DecordDecode'),
@@ -130,5 +139,3 @@ find_unused_parameters = True
 #       or not by default.
 #   - `base_batch_size` = (8 GPUs) x (12 samples per GPU).
 auto_scale_lr = dict(enable=False, base_batch_size=96)
-
-
