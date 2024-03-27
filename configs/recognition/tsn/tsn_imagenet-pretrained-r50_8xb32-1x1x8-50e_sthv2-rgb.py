dee18c31e9c1b83a947b25d984c2ede24cb09241
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-03-27 22:14:22 +0900

    Updated TSN config

--- configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x8-50e_sthv2-rgb.py
+++ configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x8-50e_sthv2-rgb.py
@@ -99,7 +99,7 @@ test_evaluator = val_evaluator
 default_hooks = dict(checkpoint=dict(interval=1000, max_keep_ckpts=1))
 
 train_cfg = dict(
-    type='EpochBasedTrainLoop', max_epochs=64, val_begin=1, val_interval=5)
+    type='EpochBasedTrainLoop', max_epochs=32, val_begin=1, val_interval=5)
 
 # Default setting for scaling LR automatically
 #   - `enable` means enable scaling LR automatically
