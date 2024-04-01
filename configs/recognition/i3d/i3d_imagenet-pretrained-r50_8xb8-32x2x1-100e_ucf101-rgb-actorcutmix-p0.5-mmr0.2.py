08d0c0471051768480c0ee91f3ad996d4b55c88b
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-04-01 15:03:11 +0900

    Added I3D configs

--- configs/recognition/i3d/i3d_imagenet-pretrained-r50_8xb8-32x2x1-100e_ucf101-rgb-actorcutmix-p0.5-mmr0.2.py
+++ configs/recognition/i3d/i3d_imagenet-pretrained-r50_8xb8-32x2x1-100e_ucf101-rgb-actorcutmix-p0.5-mmr0.2.py
@@ -1,57 +1,74 @@
 _base_ = [
-    '../../_base_/models/c3d_sports1m_pretrained.py',
-    '../../_base_/default_runtime.py',
+    '../../_base_/models/i3d_r50.py', '../../_base_/schedules/sgd_100e.py',
+    '../../_base_/default_runtime.py'
 ]
 
 # dataset settings
 dataset_type = 'VideoDataset'
-dataset = 'hmdb51'
+dataset = 'ucf101'
+mix_mode = 'actorcutmix'
+min_mask_ratio = 0.2
+relevancy_model = 'all-mpnet-base-v2/'
+relevancy_thresh = 0.5
+num_workers = 12
+
 data_root = f'data/{dataset}/videos'
+video_root = f'data/{dataset}/REPP/{mix_mode}/mix/{relevancy_model}/{relevancy_thresh}'
+video_list = f'{video_root}/list.json'
 data_root_val = f'data/{dataset}/videos'
 split = 1  # official train/test splits. valid numbers: 1, 2, 3
-ann_file_train = f'data/{dataset}/train.txt'
-ann_file_val = f'data/{dataset}/test.txt'
-ann_file_test = f'data/{dataset}/test.txt'
-num_workers = 16
-batch_size = 64
-clip_len = 16
+ann_file_train = f'data/{dataset}/{dataset}_train_split_{split}_videos.txt'
+ann_file_val = f'data/{dataset}/{dataset}_val_split_{split}_videos.txt'
+ann_file_test = f'data/{dataset}/{dataset}_val_split_{split}_videos.txt'
 
 file_client_args = dict(io_backend='disk')
 train_pipeline = [
     dict(type='DecordInit', **file_client_args),
-    dict(type='SampleFrames', clip_len=clip_len, frame_interval=1, num_clips=1),
+    dict(type='SampleFrames', clip_len=32, frame_interval=2, num_clips=1),
     dict(type='DecordDecode'),
-    dict(type='Resize', scale=(-1, 128)),
-    dict(type='RandomCrop', size=112),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(
+        type='MultiScaleCrop',
+        input_size=224,
+        scales=(1, 0.8),
+        random_crop=False,
+        max_wh_scale_gap=0),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
     dict(type='Flip', flip_ratio=0.5),
     dict(type='FormatShape', input_format='NCTHW'),
-    dict(type='PackActionInputs'),
+    dict(type='PackActionInputs')
 ]
 val_pipeline = [
     dict(type='DecordInit', **file_client_args),
     dict(
-        type='SampleFrames', clip_len=clip_len, frame_interval=1, num_clips=1, test_mode=True
-    ),
+        type='SampleFrames',
+        clip_len=32,
+        frame_interval=2,
+        num_clips=1,
+        test_mode=True),
     dict(type='DecordDecode'),
-    dict(type='Resize', scale=(-1, 128)),
-    dict(type='CenterCrop', crop_size=112),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='CenterCrop', crop_size=224),
     dict(type='FormatShape', input_format='NCTHW'),
-    dict(type='PackActionInputs'),
+    dict(type='PackActionInputs')
 ]
 test_pipeline = [
     dict(type='DecordInit', **file_client_args),
     dict(
-        type='SampleFrames', clip_len=clip_len, frame_interval=1, num_clips=10, test_mode=True
-    ),
+        type='SampleFrames',
+        clip_len=32,
+        frame_interval=2,
+        num_clips=10,
+        test_mode=True),
     dict(type='DecordDecode'),
-    dict(type='Resize', scale=(-1, 128)),
-    dict(type='CenterCrop', crop_size=112),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='ThreeCrop', crop_size=256),
     dict(type='FormatShape', input_format='NCTHW'),
-    dict(type='PackActionInputs'),
+    dict(type='PackActionInputs')
 ]
 
 train_dataloader = dict(
-    batch_size=batch_size,
+    batch_size=8,
     num_workers=num_workers,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
@@ -59,11 +76,10 @@ train_dataloader = dict(
         type=dataset_type,
         ann_file=ann_file_train,
         data_prefix=dict(video=data_root),
-        pipeline=train_pipeline,
-    ),
-)
+        pipeline=train_pipeline))
+
 val_dataloader = dict(
-    batch_size=batch_size,
+    batch_size=8,
     num_workers=num_workers,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
@@ -72,9 +88,7 @@ val_dataloader = dict(
         ann_file=ann_file_val,
         data_prefix=dict(video=data_root_val),
         pipeline=val_pipeline,
-        test_mode=True,
-    ),
-)
+        test_mode=True))
 test_dataloader = dict(
     batch_size=1,
     num_workers=num_workers,
@@ -85,36 +99,15 @@ test_dataloader = dict(
         ann_file=ann_file_test,
         data_prefix=dict(video=data_root_val),
         pipeline=test_pipeline,
-        test_mode=True,
-    ),
-)
+        test_mode=True))
 
 val_evaluator = dict(type='AccMetric')
 test_evaluator = val_evaluator
-train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=100, val_begin=1, val_interval=1)
-val_cfg = dict(type='ValLoop')
-test_cfg = dict(type='TestLoop')
-
-param_scheduler = [
-    dict(
-        type='MultiStepLR',
-        begin=0,
-        end=45,
-        by_epoch=True,
-        milestones=[20, 40],
-        gamma=0.1,
-    )
-]
-
-optim_wrapper = dict(
-    optimizer=dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005),
-    clip_grad=dict(max_norm=40, norm_type=2),
-)
 
 default_hooks = dict(checkpoint=dict(interval=1000, max_keep_ckpts=1))
 
 # Default setting for scaling LR automatically
 #   - `enable` means enable scaling LR automatically
 #       or not by default.
-#   - `base_batch_size` = (8 GPUs) x (30 samples per GPU).
-auto_scale_lr = dict(enable=False, base_batch_size=240)
+#   - `base_batch_size` = (8 GPUs) x (8 samples per GPU).
+auto_scale_lr = dict(enable=False, base_batch_size=64)
