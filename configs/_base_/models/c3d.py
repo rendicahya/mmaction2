9f1ab4bc7a5074d3a909736e002c1bca39edcc56
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-04-06 06:19:12 +0900

    Trying train from scratch

---
+++ configs/_base_/models/c3d.py
@@ -0,0 +1,26 @@
+# model settings
+model = dict(
+    type='Recognizer3D',
+    backbone=dict(
+        type='C3D',
+        style='pytorch',
+        conv_cfg=dict(type='Conv3d'),
+        norm_cfg=None,
+        act_cfg=dict(type='ReLU'),
+        dropout_ratio=0.5,
+        init_std=0.005),
+    cls_head=dict(
+        type='I3DHead',
+        num_classes=101,
+        in_channels=4096,
+        spatial_type=None,
+        dropout_ratio=0.5,
+        init_std=0.01,
+        average_clips='prob'),
+    data_preprocessor=dict(
+        type='ActionDataPreprocessor',
+        mean=[104, 117, 128],
+        std=[1, 1, 1],
+        format_shape='NCTHW'),
+    train_cfg=None,
+    test_cfg=None)
