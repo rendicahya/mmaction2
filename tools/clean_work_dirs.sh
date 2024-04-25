91ad04a227dbbca3ee2b0d3b404fef707d72e03a
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-04-25 14:31:35 +0900

    Bash script for cleaning work_dirs

---
+++ tools/clean_work_dirs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+script_dir="$(dirname "$0")"
+
+cd "$script_dir/../work_dirs" || exit
+
+find . -type f -name "last_checkpoint" | while read -r checkpoint_file; do
+    checkpoint_path=$(head -n 1 "$checkpoint_file" | tr -d '\n\r')
+    
+    if [[ -f "$checkpoint_path" ]]; then
+        rm "$checkpoint_path"
+    fi
+done
+
+cd - || exit
