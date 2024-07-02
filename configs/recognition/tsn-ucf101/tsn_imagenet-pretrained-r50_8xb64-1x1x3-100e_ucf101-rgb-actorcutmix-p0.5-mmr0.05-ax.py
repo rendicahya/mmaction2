bde881449ea3b3d98b2683e9328a015e2c343d13
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-07-02 21:10:03 +0900

    Cleaning up

--- configs/recognition/tsn-ucf101/tsn_imagenet-pretrained-r50_8xb64-1x1x3-100e_ucf101-rgb-actorcutmix-p0.5-mmr0.05-ax.py
+++ configs/recognition/tsn-ucf101/tsn_imagenet-pretrained-r50_8xb64-1x1x3-100e_ucf101-rgb-actorcutmix-p0.5-mmr0.05-ax.py
@@ -3,30 +3,27 @@ _base_ = [
     '../../_base_/default_runtime.py'
 ]
 
-# dataset settings
 dataset_type = 'VideoDataset'
 dataset = 'ucf101'
-mix_mode = 'intercutmix'
+mix_mode = 'actorcutmix'
 detector = 'UniDet'
-min_mask_ratio = 0.5
+min_mask_ratio = 0.05
 mix_prob = 0.5
-relevancy_model = 'all-mpnet-base-v2/'
-relevancy_thresh = 0.5
 num_workers = 16
 batch_size = 64
 
-data_root = f'data/{dataset}/videos'
-video_dir = f'data/{dataset}/{detector}/select/{mix_mode}/REPP/mix-0/{relevancy_model}/{relevancy_thresh}'
-data_root_val = f'data/{dataset}/videos'
+video_root = f'data/{dataset}/videos'
+class_index = f'data/{dataset}/annotations/classInd.txt'
+mix_video_dir = f'data/{dataset}/{detector}/select/{mix_mode}/REPP/mix-0'
+video_root_val = video_root
 split = 1  # official train/test splits. valid numbers: 1, 2, 3
 ann_file_train = f'data/{dataset}/{dataset}_train_split_{split}_videos.txt'
 ann_file_val = f'data/{dataset}/{dataset}_val_split_{split}_videos.txt'
 ann_file_test = f'data/{dataset}/{dataset}_val_split_{split}_videos.txt'
 
 file_client_args = dict(io_backend='disk')
-
 train_pipeline = [
-    dict(type='InterCutMix', video_dir=video_dir, mix_prob=mix_prob, min_mask_ratio=min_mask_ratio),
+    dict(type='ActorCutMix', mix_video_dir=mix_video_dir, class_index=class_index, mix_prob=mix_prob, min_mask_ratio=min_mask_ratio),
     dict(type='DecordInit', **file_client_args),
     dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=3),
     dict(type='DecordDecode'),
@@ -79,7 +76,7 @@ train_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         ann_file=ann_file_train,
-        data_prefix=dict(video=data_root),
+        data_prefix=dict(video=video_root),
         pipeline=train_pipeline))
 val_dataloader = dict(
     batch_size=batch_size,
@@ -89,7 +86,7 @@ val_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         ann_file=ann_file_val,
-        data_prefix=dict(video=data_root_val),
+        data_prefix=dict(video=video_root_val),
         pipeline=val_pipeline,
         test_mode=True))
 test_dataloader = dict(
@@ -100,7 +97,7 @@ test_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         ann_file=ann_file_val,
-        data_prefix=dict(video=data_root_val),
+        data_prefix=dict(video=video_root_val),
         pipeline=test_pipeline,
         test_mode=True))
 
--- configs/recognition/tsn-ucf101/tsn_imagenet-pretrained-r50_8xb64-1x1x3-100e_ucf101-rgb-intercutmix-p0.5-mmr0.05-ax.py
+++ configs/recognition/tsn-ucf101/tsn_imagenet-pretrained-r50_8xb64-1x1x3-100e_ucf101-rgb-intercutmix-p0.5-mmr0.05-ax.py
@@ -5,25 +5,27 @@ _base_ = [
 
 dataset_type = 'VideoDataset'
 dataset = 'ucf101'
-mix_mode = 'actorcutmix'
+mix_mode = 'intercutmix'
 detector = 'UniDet'
 min_mask_ratio = 0.05
 mix_prob = 0.5
+relevancy_model = 'all-mpnet-base-v2/'
+relevancy_thresh = 0.5
 num_workers = 16
 batch_size = 64
 
-data_root = f'data/{dataset}/videos'
-video_dir = f'data/{dataset}/{detector}/select/{mix_mode}/REPP/mix-0'
-data_root_val = f'data/{dataset}/videos'
+video_root = f'data/{dataset}/videos'
+class_index = f'data/{dataset}/annotations/classInd.txt'
+mix_video_dir = f'data/{dataset}/{detector}/select/{mix_mode}/REPP/mix-0/{relevancy_model}/{relevancy_thresh}'
+video_root_val = video_root
 split = 1  # official train/test splits. valid numbers: 1, 2, 3
 ann_file_train = f'data/{dataset}/{dataset}_train_split_{split}_videos.txt'
 ann_file_val = f'data/{dataset}/{dataset}_val_split_{split}_videos.txt'
 ann_file_test = f'data/{dataset}/{dataset}_val_split_{split}_videos.txt'
 
 file_client_args = dict(io_backend='disk')
-
 train_pipeline = [
-    dict(type='ActorCutMix', video_dir=video_dir, mix_prob=mix_prob, min_mask_ratio=min_mask_ratio),
+    dict(type='InterCutMix', mix_video_dir=mix_video_dir, class_index=class_index, mix_prob=mix_prob, min_mask_ratio=min_mask_ratio),
     dict(type='DecordInit', **file_client_args),
     dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=3),
     dict(type='DecordDecode'),
@@ -76,7 +78,7 @@ train_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         ann_file=ann_file_train,
-        data_prefix=dict(video=data_root),
+        data_prefix=dict(video=video_root),
         pipeline=train_pipeline))
 val_dataloader = dict(
     batch_size=batch_size,
@@ -86,7 +88,7 @@ val_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         ann_file=ann_file_val,
-        data_prefix=dict(video=data_root_val),
+        data_prefix=dict(video=video_root_val),
         pipeline=val_pipeline,
         test_mode=True))
 test_dataloader = dict(
@@ -97,7 +99,7 @@ test_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         ann_file=ann_file_val,
-        data_prefix=dict(video=data_root_val),
+        data_prefix=dict(video=video_root_val),
         pipeline=test_pipeline,
         test_mode=True))
 
