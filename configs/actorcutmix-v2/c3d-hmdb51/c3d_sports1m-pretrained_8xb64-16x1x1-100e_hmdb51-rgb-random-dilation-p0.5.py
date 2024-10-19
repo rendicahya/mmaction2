0c68d3386a52d681105f9882c337a162e35d463e
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-10-19 11:39:35 +0900

    Fixed stupid mistake

--- configs/actorcutmix-v2/c3d-hmdb51/c3d_sports1m-pretrained_8xb64-16x1x1-100e_hmdb51-rgb-random-dilation-p0.5.py
+++ configs/actorcutmix-v2/c3d-hmdb51/c3d_sports1m-pretrained_8xb64-16x1x1-100e_hmdb51-rgb-random-dilation-p0.5.py
@@ -5,7 +5,7 @@ _base_ = [
 
 dataset_type = 'VideoDataset'
 dataset = 'hmdb51'
-scene_selection = 'random-closing'
+scene_selection = 'random-dilation'
 detector = 'yolov8-coco'
 mask_dir_name = 'mask-dilation'
 detection_conf = 0.25
